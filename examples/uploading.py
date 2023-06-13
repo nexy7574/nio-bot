e2ec27045a831986a880a5f6895ed91761e1b699
nex <me@nexy7574.co.uk>
2023-06-13 21:48:42 +0100

    Add examples (untested)

---
+++ examples/uploading.py
@@ -0,0 +1,36 @@
+import niobot
+import aiohttp
+import io
+import shutil
+
+bot = niobot.NioBot(
+    "https://matrix.org",
+    "@user:matrix.org",
+    command_prefix="£"
+)
+
+
+@bot.command("upload")
+async def upload_random_image(ctx: niobot.Context):
+    """Uploads a random inspirational quote!"""
+    if not shutil.which("ffprobe"):
+        await ctx.reply("ffprobe is not installed! This command requires ffprobe (part of ffmpeg) to work.")
+        return
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://inspirobot.me/api?generate=true") as resp:
+            if resp.status != 200:
+                await ctx.reply("Failed to fetch image!")
+                return
+            url = await resp.text()
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    await ctx.reply("Failed to fetch image!")
+                    return
+                with open("./tmp-image.png", "w+") as file:
+                    file.write(await resp.read())
+                attachment = await niobot.MediaAttachment.from_file("./tmp-image.png")
+                await ctx.reply("Here's your image!", file=attachment)
+
+
+bot.run(password="my_password")
