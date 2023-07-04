60bba18cd2543c539c076a0db91202342c02a6a9
nex <me@nexy7574.co.uk>
2023-07-04 21:36:37 +0100

    Fix inconsistent password flag

--- src/niobot/__main__.py
+++ src/niobot/__main__.py
@@ -334,7 +334,7 @@ def new():
 
 @new.command()
 @click.argument("path", type=click.Path())
-@click.option("--password", "-p", prompt=True, hide_input=True)
+@click.option("--password", "-P", prompt=True, hide_input=True)
 @click.option("--user-id", "-U", prompt=True)
 @click.option("--homeserver", "-H", prompt=True)
 @click.option("--device-id", "-D", prompt=True)
