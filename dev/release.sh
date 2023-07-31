3cccd304c4432228940a2dfcf3b1e9b530df7925
nex <me@nexy7574.co.uk>
2023-07-31 18:40:22 +0100

    fix script pusher

--- dev/release.sh
+++ dev/release.sh
@@ -3,35 +3,35 @@
 set -e
 printf "Enter the version number: "
 read -r VERSION
-echo '\nEnsuring version is pep440-compliant...'
+printf '\nEnsuring version is pep440-compliant...\n'
 python3 -m pep440 "$VERSION"
 VERSION_P="v$VERSION"
 
-echo '\nPerforming last-minute formatting...'
+printf '\nPerforming last-minute formatting...\n'
 black src
 isort src
 git add . || true
 git commit -am "Format code for release $VERSION" || true
 
-echo '\nCreating release branch...'
+printf '\nCreating release branch...\n'
 git tag -m "Release $VERSION_P" "$VERSION"
 git checkout -b release/"$VERSION_P"
 
-echo '\nBuilding python wheel & source dist...'
+printf '\nBuilding python wheel & source dist...\n'
 python3 -m build
 
 #echo '\nBuilding docker image...'
 #docker buildx build -t nexy7574/nio-bot:"$VERSION" --platform linux/amd64,linux/arm64 --load .
 #echo "\nDocker image built & imported. You should 'docker image push n3xy7574/nio-bot:$VERSION'."
 
-echo '\nFinalising & pushing release...'
-git push --tags origin release/"$VERSION"
+printf '\nFinalising & pushing release...\n'
+git push --all origin release/"$VERSION"
 
-echo '\nSwitching back to master for further development...'
+printf '\nSwitching back to master for further development...\n'
 git checkout master
 
-echo '\nOpening browser so that you can upload the wheels to the tag'
+printf '\nOpening browser so that you can upload the wheels to the tag\n'
 xdg-open file://"$PWD"/dist &
 xdg-open "https://github.com/EEKIM10/niobot/releases/edit/$VERSION" &
 
-echo '\nDone!'
+printf '\nDone!\n'
