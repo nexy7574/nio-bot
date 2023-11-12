7a65b4a1dcce135ba73326c9e33ac501983c3be6
nex <me@nexy7574.co.uk>
2023-11-12 14:53:29 +0000

    Add more tests

--- src/tests/test_attachment.py
+++ src/tests/test_attachment.py
@@ -1,4 +1,5 @@
 import pathlib
+import typing
 
 import pytest
 
@@ -12,7 +13,7 @@ ASSETS = pathlib.Path(__file__).parent / "assets"
     [
         (ASSETS / "sample-5s-compressed.mp4", niobot.VideoAttachment),
         (ASSETS / "sample-15s.ogg", niobot.AudioAttachment),
-        (ASSETS / "sample-clouds.avif", niobot.ImageAttachment),
+        (ASSETS / "sample-clouds.webp", niobot.ImageAttachment),
         (ASSETS / "sample-lipsum-10.txt", niobot.FileAttachment),
     ],
 )
@@ -25,9 +26,43 @@ def test_which(file_path: pathlib.Path, expected):
     [
         (ASSETS / "sample-5s-compressed.mp4", "video/mp4"),
         (ASSETS / "sample-15s.ogg", "audio/ogg"),
-        (ASSETS / "sample-clouds.avif", "image/avif"),
+        (ASSETS / "sample-clouds.webp", "image/webp"),
         (ASSETS / "sample-lipsum-10.txt", "text/csv"),  # while it is a text file, this does technically yield csv.
     ],
 )
 def test_mimetype_detection(file_path: pathlib.Path, expected: str):
     assert niobot.detect_mime_type(file_path) == expected, str(file_path)
+
+
+@pytest.mark.parametrize(
+    "file_path, expected_type, expected_values",
+    [
+        (ASSETS / "sample-15s.ogg", niobot.AudioAttachment, {"duration": 19.18 * 1000}),
+        (
+                ASSETS / "sample-5s-compressed.mp4",
+                niobot.VideoAttachment,
+                {
+                    "duration": 5758,
+                    "height": 1080,
+                    "width": 1920,
+                }
+        ),
+        (
+            ASSETS / "sample-clouds.webp",
+            niobot.ImageAttachment,
+            {
+                "height": 300,
+                "width": 400,
+            }
+        )
+    ]
+)
+@pytest.mark.asyncio
+async def test_rich_data_detection(file_path: pathlib.Path, expected_type, expected_values: dict[str, typing.Any]):
+    _type = niobot.which(file_path)
+    assert _type == expected_type, "%s was %r, not %r." % (str(file_path), _type, expected_type)
+    instance = await _type.from_file(file_path)
+
+    for key, value in expected_values.items():
+        attr = getattr(instance, key)
+        assert attr == value, f"{key} does not match (expected {value!r}, got {attr!r})"
---
+++ src/tests/test_client.py
@@ -0,0 +1,66 @@
+import re
+import typing
+
+import niobot
+import pytest
+
+
+DEFAULT_HELP_COMMAND = niobot.Command(
+    "help",
+    niobot.default_help_command,
+    aliases=["h"],
+    description="Shows a list of commands for this bot",
+)
+
+
+@pytest.mark.parametrize(
+    "kwargs,expected_error,expected_attrs",
+    [
+        ({"homeserver": "https://example.com", "user_id": "@example:example.com", "command_prefix": "!"}, None, ...),
+        (
+            {
+                "homeserver": "https://example.com",
+                "user_id": "@example:example.com",
+                "command_prefix": re.compile(r"^!"),
+            },
+            None,
+            ...,
+        ),
+        (
+            {"homeserver": "https://example.com", "user_id": "@example:example.com", "command_prefix": "foo bar"},
+            RuntimeError,
+            ...,
+        ),
+        (
+            {"homeserver": "https://example.com", "user_id": "@example:example.com", "command_prefix": None},
+            TypeError,
+            ...,
+        ),
+        (
+            {
+                "homeserver": "https://example.com",
+                "user_id": "@example:example.com",
+                "command_prefix": re.compile(r"^!"),
+                "help_command": "none",
+            },
+            TypeError,
+            ...,
+        ),
+    ],
+)
+def test_client_init(
+    kwargs,
+    expected_error: typing.Optional[type(Exception)],
+    expected_attrs: typing.Union[dict[str, typing.Any], Ellipsis],
+):
+    if expected_error is not None:
+        with pytest.raises(expected_error):
+            niobot.NioBot(**kwargs)
+    else:
+        if expected_attrs is ...:
+            expected_attrs = kwargs
+        client = niobot.NioBot(**kwargs)
+        for expected_attr_name, expected_attr_value in expected_attrs.items():
+            assert (
+                getattr(client, expected_attr_name) == expected_attr_value
+            ), f"Attribute {expected_attr_name} is not {expected_attr_value}"
