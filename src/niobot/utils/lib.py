9739f8adaa9f02df6ad1527f606b658c65b9ea9c
nexy7574 <git@nexy7574.co.uk>
2024-08-25 23:18:58 +0100

    Attempt to use python 3.13 warnings.deprecated, fall back otherwise

--- src/niobot/utils/lib.py
+++ src/niobot/utils/lib.py
@@ -5,23 +5,31 @@ import warnings
 __all__ = ("deprecated", "silence_noisy_loggers")
 
 
-def deprecated(use_instead: str = None):
-    """Marks a function as deprecated and will warn users on call."""
+try:
+    from warnings import deprecated
+    # Python 3.13 and above
+except ImportError:
+    # Below python 3.13, we need to define our own deprecated decorator
+    def deprecated(use_instead: str = None):
+        """Marks a function as deprecated and will warn users on call."""
 
-    def wrapper(func):
-        @functools.wraps(func)
-        def caller(*args, **kwargs):
-            value = "{} is deprecated.{}".format(
-                func.__qualname__, "" if not use_instead else " Please use %r instead." % use_instead
-            )
-            warn = DeprecationWarning(value)
-            warnings.warn(warn)
-            return func(*args, **kwargs)
+        def wrapper(func):
+            @functools.wraps(func)
+            def caller(*args, **kwargs):
+                value = "{} is deprecated.{}".format(
+                    func.__qualname__, "" if not use_instead else " Please use %r instead." % use_instead
+                )
+                warnings.warn(
+                    value,
+                    category=DeprecationWarning,
+                    stacklevel=2,
+                )
+                return func(*args, **kwargs)
 
-        caller.__doc__ = func.__doc__
-        return caller
+            caller.__doc__ = func.__doc__
+            return caller
 
-    return wrapper
+        return wrapper
 
 
 def silence_noisy_loggers(*exclude: str):
