740fd2216b32b115d43c0ff2839f95a58631d0b9
nexy7574 <git@nexy7574.co.uk>
2024-11-25 01:42:06 +0000

    Remove benchmark and add coverage

--- src/tests/test_utils_string_view.py
+++ src/tests/test_utils_string_view.py
@@ -3,47 +3,18 @@ import pytest
 from niobot.utils.string_view import ArgumentView
 
 STRINGS = [
-    [
-        "hello world",
-        ["hello", "world"]
-    ],
-    [
-        "hello 'world'",
-        ["hello", "world"]
-    ],
-    [
-        "'hello world'",
-        ["hello world"]
-    ],
-    [
-        "hello 'world' 'two'",
-        ["hello", "world", "two"]
-    ],
-    [
-        "hello 'world two'",
-        ["hello", "world two"]
-    ],
-    [
-        "hello 'world two' three",
-        ["hello", "world two", "three"]
-    ],
-    [
-        "\"hello 'world'\"",
-        ["hello 'world'"]
-    ],
-    [
-        """Hello "'`world`'"!""",
-        ["Hello", "'`world`'", "!"]
-    ],
-    [
-        '''"""hello world"""''',
-        ["hello world"]
-    ],
-    [
-        """'''hello world''""",
-        ["hello world"]
-    ]
+    ["hello world", ["hello", "world"]],
+    ["hello 'world'", ["hello", "world"]],
+    ["'hello world'", ["hello world"]],
+    ["hello 'world' 'two'", ["hello", "world", "two"]],
+    ["hello 'world two'", ["hello", "world two"]],
+    ["hello 'world two' three", ["hello", "world two", "three"]],
+    ["\"hello 'world'\"", ["hello 'world'"]],
+    ["""Hello "'`world`'"!""", ["Hello", "'`world`'", "!"]],
+    ['''"""hello world"""''', ["hello world"]],
+    ["""'''hello world''""", ["hello world"]],
 ]
+
 
 @pytest.mark.parametrize("string, expected", STRINGS)
 def test_string_view(string, expected):
