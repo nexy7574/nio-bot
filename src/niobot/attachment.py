1a40a87a839557d43eaf4f3d92d1f0c9e5dc478d
nexy7574 <git@nexy7574.co.uk>
2024-12-24 14:17:33 +0000

    Fix some shadowing var issues

--- src/niobot/attachment.py
+++ src/niobot/attachment.py
@@ -46,11 +46,11 @@ __all__ = (
 
 
 @deprecated("<T>Attachment.get_metadata")
-def get_metadata_ffmpeg(file: Union[str, pathlib.Path]) -> Dict[str, Any]:
+def get_metadata_ffmpeg(file_: Union[str, pathlib.Path]) -> Dict[str, Any]:
     # Deprecated, will be removed in v1.4.0
     if not shutil.which("ffprobe"):
         raise FileNotFoundError("ffprobe is not installed. If it is, check your $PATH.")
-    command = ["ffprobe", "-of", "json", "-loglevel", "9", "-show_format", "-show_streams", "-i", str(file)]
+    command = ["ffprobe", "-of", "json", "-loglevel", "9", "-show_format", "-show_streams", "-i", str(file_)]
     start = time.perf_counter()
     try:
         result = subprocess.run(command, capture_output=True, encoding="utf-8", errors="replace", check=True)
@@ -64,10 +64,10 @@ def get_metadata_ffmpeg(file: Union[str, pathlib.Path]) -> Dict[str, Any]:
 
 
 @deprecated("<T>Attachment.get_metadata")
-def get_metadata_imagemagick(file: pathlib.Path) -> Dict[str, Any]:
+def get_metadata_imagemagick(file_: pathlib.Path) -> Dict[str, Any]:
     # Deprecated, will be removed in v1.4.0
-    file = file.resolve(True)
-    command = ["identify", "-format", "%m,%w,%h", str(file)]
+    file_ = file_.resolve(True)
+    command = ["identify", "-format", "%m,%w,%h", str(file_)]
     start = time.perf_counter()
     try:
         result = subprocess.run(command, capture_output=True, encoding="utf-8", errors="replace", check=True)
@@ -76,7 +76,7 @@ def get_metadata_imagemagick(file: pathlib.Path) -> Dict[str, Any]:
     log.debug("identify output (%d): %s", result.returncode, result.stdout)
     log.debug("identify took %f seconds", time.perf_counter() - start)
     stdout = result.stdout
-    stdout = stdout[len(str(file)) + 1 :]
+    stdout = stdout[len(str(file_)) + 1:]
     img_format, img_width, img_height = stdout.split(",")
     data = {
         "streams": [
@@ -90,9 +90,9 @@ def get_metadata_imagemagick(file: pathlib.Path) -> Dict[str, Any]:
             },
         ],
         "format": {
-            "filename": str(file),
+            "filename": str(file_),
             "format_long_name": img_format,
-            "size": str(file.stat().st_size),
+            "size": str(file_.stat().st_size),
         },
     }
     log.debug("Parsed identify output:\n%s", json.dumps(data, indent=4))
@@ -100,16 +100,16 @@ def get_metadata_imagemagick(file: pathlib.Path) -> Dict[str, Any]:
 
 
 @deprecated("<T>Attachment.get_metadata")
-def get_metadata(file: Union[str, pathlib.Path], mime_type: Optional[str] = None) -> Dict[str, Any]:
+def get_metadata(file_: Union[str, pathlib.Path], mime_type: Optional[str] = None) -> Dict[str, Any]:
     # Deprecated, will be removed in v1.4.0
-    file = _to_path(file)
-    mime = mime_type or detect_mime_type(file)
+    file_ = _to_path(file_)
+    mime = mime_type or detect_mime_type(file_)
     mime = mime.split("/")[0]
     if mime == "image":
         # First, try using PIL to get the metadata
         try:
-            log.debug("Using PIL to detect metadata for %r", file)
-            with PIL.Image.open(file) as img:
+            log.debug("Using PIL to detect metadata for %r", file_)
+            with PIL.Image.open(file_) as img:
                 data = {
                     "streams": [
                         {
@@ -122,15 +122,15 @@ def get_metadata(file: Union[str, pathlib.Path], mime_type: Optional[str] = None
                         },
                     ],
                     "format": {
-                        "filename": str(file),
+                        "filename": str(file_),
                         "format_long_name": img.format,
-                        "size": file.stat().st_size,
+                        "size": file_.stat().st_size,
                     },
                 }
-                log.debug("PIL metadata for %r: %r", file, data)
+                log.debug("PIL metadata for %r: %r", file_, data)
                 return data
         except (PIL.UnidentifiedImageError, OSError):
-            log.warning("Failed to detect metadata for %r with PIL. Falling back to imagemagick.", file, exc_info=True)
+            log.warning("Failed to detect metadata for %r with PIL. Falling back to imagemagick.", file_, exc_info=True)
             if not shutil.which("identify"):
                 log.warning(
                     "Imagemagick identify not found, falling back to ffmpeg for image metadata detection. "
@@ -140,35 +140,35 @@ def get_metadata(file: Union[str, pathlib.Path], mime_type: Optional[str] = None
                 start = time.perf_counter()
 
                 try:
-                    r = get_metadata_imagemagick(file)
+                    r = get_metadata_imagemagick(file_)
                     log.debug("get_metadata_imagemagick took %f seconds", time.perf_counter() - start)
-                    log.debug("identify detected data for %r: %r", file, r)
+                    log.debug("identify detected data for %r: %r", file_, r)
                     return r
                 except (IndexError, ValueError, subprocess.SubprocessError, OSError):
                     log.warning(
                         "Failed to detect metadata for %r with imagemagick. Falling back to ffmpeg.",
-                        file,
+                        file_,
                         exc_info=True,
                     )
 
     if mime not in ["audio", "video", "image"]:
         raise MetadataDetectionException("Unsupported mime type. Must be an audio clip, video, or image.")
     start = time.perf_counter()
-    log.debug("Getting metadata for %r with ffprobe", file)
-    r = get_metadata_ffmpeg(file)
+    log.debug("Getting metadata for %r with ffprobe", file_)
+    r = get_metadata_ffmpeg(file_)
     log.debug("get_metadata_ffmpeg took %f seconds", time.perf_counter() - start)
-    log.debug("ffprobe detected data for %r: %r", file, r)
+    log.debug("ffprobe detected data for %r: %r", file_, r)
     return r
 
 
 @deprecated("VideoAttachment.first_frame")
-def first_frame(file: Union[str, pathlib.Path], file_format: str = "webp") -> bytes:
+def first_frame(file_: Union[str, pathlib.Path], file_format: str = "webp") -> bytes:
     # Deprecated, will be removed in v1.4.0
     if not shutil.which("ffmpeg"):
         raise FileNotFoundError("ffmpeg is not installed. If it is, check your $PATH.")
     with tempfile.NamedTemporaryFile(suffix=f".{file_format}") as f:
-        command = ["ffmpeg", "-loglevel", "9", "-i", str(file), "-frames:v", "1", "-y", "-strict", "-2", f.name]
-        log.debug("Extracting first frame of %r: %s", file, " ".join(command))
+        command = ["ffmpeg", "-loglevel", "9", "-i", str(file_), "-frames:v", "1", "-y", "-strict", "-2", f.name]
+        log.debug("Extracting first frame of %r: %s", file_, " ".join(command))
         try:
             start = time.perf_counter()
             log.debug("Extraction return code: %d", subprocess.run(command, capture_output=True, check=True).returncode)
@@ -180,7 +180,7 @@ def first_frame(file: Union[str, pathlib.Path], file_format: str = "webp") -> by
 
 
 def which(
-    file: Union[io.BytesIO, pathlib.Path, str],
+    file_: Union[io.BytesIO, pathlib.Path, str],
     mime_type: Optional[str] = None,
 ) -> Union[
     Type[FileAttachment],
@@ -211,7 +211,7 @@ def which(
         attachment = await attachment_type.from_file(my_file)
         ```
 
-    :param file: The file or BytesIO to investigate
+    :param file_: The file or BytesIO to investigate
     :param mime_type: The optional pre-detected mime type. If this is not provided, it will be detected.
     :return: The correct type for this attachment (not instantiated)
     """
@@ -221,6 +221,6 @@ def which(
         "video": VideoAttachment,
     }
     if not mime_type:
-        mime_type = detect_mime_type(file)
+        mime_type = detect_mime_type(file_)
     mime_start = mime_type.split("/")[0].lower()
     return values.get(mime_start, FileAttachment)
